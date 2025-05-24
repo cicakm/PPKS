cb92085e6b33ed90b0b3a20f0b02327c33b7ea3d
Marin Cicak <cicakmarin4@gmail.com>
2025-05-24 18:07:41 +0200

    add simple API

--- server/index.js
+++ server/index.js
@@ -1,2 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello from server!");
+});
+
+app.listen(8080, () => {
+  console.log("Server listening  on localhost:8080");
+});
