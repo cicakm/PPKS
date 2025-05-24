cd7597f7f1560f7103c5a835bfab6b381669e8ea
Marin Cicak <cicakmarin4@gmail.com>
2025-05-24 18:08:39 +0200

    add axios library; add simple API call

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+
+function apiCall() {
+  axios.get("http://localhost:8080").then((data) => {
+    console.log(data.data);
+  });
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={apiCall}>Make API call</button>
       </header>
     </div>
   );
