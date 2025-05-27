ac28dd9d3842fc3d95cdbe5edd574812cbed3aa9
Marin Cicak <cicakmarin4@gmail.com>
2025-05-27 12:35:56 +0200

    add controllers for login and register

--- server/index.js
+++ server/index.js
@@ -1,19 +1,50 @@
-import { getAllUsers, getUser } from "./db.js";
-
+import { createUser, getUserByUsername } from "./db.js";
 import express from "express";
-const app = express();
 import cors from "cors";
 
-app.use(cors());
+const app = express();
 
-app.get("/", async (req, res) => {
-  res.send(await getAllUsers());
+app.use(cors());
+app.use(express.json());
+
+app.post("/register", async (req, res) => {
+  if (
+    req.body.name === "" ||
+    req.body.name === undefined ||
+    req.body.username === "" ||
+    req.body.username === undefined ||
+    req.body.password === "" ||
+    req.body.password === undefined ||
+    req.body.repeatPassword === "" ||
+    req.body.repeatPassword === undefined
+  ) {
+    res.status(400).send("All data must be filled!");
+  }
+
+  if (req.body.password !== req.body.repeatPassword) {
+    res.status(400).send("Password mismatch");
+  }
+
+  if (await getUserByUsername(req.body.username)) {
+    res
+      .status(400)
+      .send(`User with username: "${req.body.username}" already exists!`);
+  } else {
+    await createUser(req.body.name, req.body.username, req.body.password);
+    res.status(200).send({ name: req.body.name, username: req.body.username });
+  }
 });
 
 app.post("/login", async (req, res) => {
-  const user = await getUser(req.body.username);
-  if (user.password === req.body.password) {
-    res.send({ username: user.username });
+  const user = await getUserByUsername(req.body.username);
+  if (user) {
+    if (user.password === req.body.password) {
+      res.send({ name: user.name, username: user.username });
+    } else {
+      res.status(400).send("Username or password incorrect");
+    }
+  } else {
+    res.status(400).send("Username or password incorrect");
   }
 });
 
