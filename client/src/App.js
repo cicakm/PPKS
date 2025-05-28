43aedb16e08bbb257ee24ef2258c8a94c97376e6
Marin Cicak <cicakmarin4@gmail.com>
2025-05-28 12:07:59 +0200

    add socket.io-client to frontend; simple message sending

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,25 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/register/RegisterPage";
 import { AuthProvider } from "./context/AuthContext";
 import NavbarComponent from "./components/navbar/NavbarComponent";
+import { socket } from "./socket";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.on("message", (msg) => {
+      setMessages((prev) => [...prev, msg]);
+    });
+    return () => socket.off("message");
+  });
+
+  const sendMessage = () => {
+    socket.emit("message", message);
+    setMessage("");
+  };
+
   return (
     <>
       <AuthProvider>
@@ -20,6 +37,16 @@ function App() {
             <Route path="/register" element={<RegisterPage />} />
           </Routes>
         </Router>
+
+        <div>
+          <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} />
+          <button onClick={sendMessage}>Send</button>
+          <ul>
+            {messages.map((msg, i) => (
+              <li key={i}>{msg}</li>
+            ))}
+          </ul>
+        </div>
       </AuthProvider>
     </>
   );
