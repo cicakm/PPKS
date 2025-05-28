5fb80c2e9372dcdd59ebc64c4e58d5d0182dc744
Marin Cicak <cicakmarin4@gmail.com>
2025-05-28 18:38:52 +0200

    fix bugs with fetching from database

--- server/db.js
+++ server/db.js
@@ -37,7 +37,7 @@ async function getUserIDByUsername(username) {
       "select id from myuser where username = $1",
       [username]
     );
-    return result.rows[0];
+    return result.rows[0].id;
   } catch (error) {
     console.log(error);
   }
@@ -45,9 +45,9 @@ async function getUserIDByUsername(username) {
 
 export async function saveMessage(from, to, message) {
   try {
-    const fromID = getUserIDByUsername(from);
+    const fromID = await getUserIDByUsername(from);
     if (!fromID) return `User with username: ${from} does not exist!`;
-    const toID = getUserIDByUsername(to);
+    const toID = await getUserIDByUsername(to);
     if (!toID) return `User with username: ${to} does not exist!`;
 
     await pool.query(
