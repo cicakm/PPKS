bc501e1a9ae0431a8a87534f04c0c8275ca8a642
Marin Cicak <cicakmarin4@gmail.com>
2025-05-28 16:23:20 +0200

    add chatlist link to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import NavbarComponent from "./components/navbar/NavbarComponent";
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/login/LoginPage";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/register/RegisterPage";
+import ChatListPage from "./pages/chatlist/ChatListPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import NavbarComponent from "./components/navbar/NavbarComponent";
-import { socket } from "./socket";
-import { useState, useEffect } from "react";
 
 function App() {
-  const [message, setMessage] = useState("");
-  const [messages, setMessages] = useState([]);
-
-  useEffect(() => {
-    socket.on("message", (msg) => {
-      setMessages((prev) => [...prev, msg]);
-    });
-    return () => socket.off("message");
-  });
-
-  const sendMessage = () => {
-    socket.emit("message", message);
-    setMessage("");
-  };
-
   return (
     <>
       <AuthProvider>
         <NavbarComponent />
-
         <Router>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
+            <Route path="/chat" element={<ChatListPage />} />
           </Routes>
         </Router>
-
-        <div>
-          <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} />
-          <button onClick={sendMessage}>Send</button>
-          <ul>
-            {messages.map((msg, i) => (
-              <li key={i}>{msg}</li>
-            ))}
-          </ul>
-        </div>
       </AuthProvider>
     </>
   );
