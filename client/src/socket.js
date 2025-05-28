89f63ec460f9c19c00c1d5a56cad28bc54712b61
Marin Cicak <cicakmarin4@gmail.com>
2025-05-28 16:23:42 +0200

    add attributes to socket

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,2 +1,5 @@
 import { io } from "socket.io-client";
-export const socket = io("http://localhost:8080");
+export const socket = io("http://localhost:8080", {
+  autoConnect: false,
+  closeOnBeforeunload: false,
+});
