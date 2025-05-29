973dcc202a82ba4fc595d497471017057b2b25b9
Marin Cicak <cicakmarin4@gmail.com>
2025-05-29 12:45:24 +0200

    add user chats page

--- client/src/pages/chatlist/ChatListPage.jsx
+++ client/src/pages/chatlist/ChatListPage.jsx
@@ -1,28 +1,59 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ChatPage from "./chat/ChatPage";
+import { ListGroup, Container, Row, Col } from "react-bootstrap";
+import axios from "axios";
 
 const ChatListPage = () => {
-  const [user, setUser] = useState("");
-  const [show, setShow] = useState(false);
+
+  const [chats, setChats] = useState({});
+
+  const [selectedUser, setSelectedUser] = useState(null);
+  const selected = Object.entries(chats).find(
+    (chat) => chat[1].username === selectedUser
+  );
+
+  useEffect(() => {
+    axios
+      .post("http://localhost:8080/chats", {
+        from: localStorage.getItem("username"),
+      })
+      .then((res) => {
+        setChats(res.data);
+      });
+  }, []);
 
   return (
     <>
-      {show && (
-        <ChatPage
-          currentUser={localStorage.getItem("username")}
-          otherUser={user}
-        />
-      )}
-      <div>
-        <input
-          type="text"
-          placeholder="To"
-          value={user}
-          onChange={(e) => setUser(e.target.value)}
-        />
-        <button onClick={() => setShow(true)}>Show</button>
-      </div>
-    </>
+      <Container style={{ maxWidth: 500}}>
+        <Row>
+          <Col>
+            {!selected ? (
+              <ListGroup>
+                {Object.entries(chats).map((chat) => (
+                  <>
+                    <ListGroup.Item
+                      key={chat[0]}
+                      action
+                      active={selectedUser === chat[1].username}
+                      onClick={() => setSelectedUser(chat[1].username)}
+                      style={{ cursor: "pointer" }}
+                    >
+                      {chat[1].username}
+                    </ListGroup.Item>
+                  </>
+                ))}
+              </ListGroup>
+            ) : (
+              <ChatPage
+                currentUser={localStorage.getItem("username")}
+                otherUser={selectedUser}
+                onBack={() => setSelectedUser(null)}
+              />
+            )}
+          </Col>
+        </Row>
+      </Container>
+</>
   );
 };
 
