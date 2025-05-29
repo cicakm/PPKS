5049700b0a12426fd1cdfee4d72b204a5390228b
Marin Cicak <cicakmarin4@gmail.com>
2025-05-29 22:30:57 +0200

    separate data validation

---
+++ server/middleware/validate.js
@@ -0,0 +1,12 @@
+export const validateRegister = (req, res, next) => {
+  const { name, username, password, repeatPassword } = req.body;
+
+  if (!name || !username || !password || !repeatPassword) {
+    return res.status(400).json({ message: "All data must be filled!" });
+  }
+
+  if (req.body.password !== req.body.repeatPassword) {
+    return res.status(400).json({ message: "Password mismatch" });
+  }
+  next();
+};
